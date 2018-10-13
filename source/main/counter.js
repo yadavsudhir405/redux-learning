4d68649c7376a8746c3a59939b2543b66dff0210
Sudhir Yadav <yadavsudhir405@gmail.com>
2018-10-13 15:58:30 +0530

    Used createStore function of redux

---
+++ source/main/counter.js
@@ -0,0 +1,11 @@
+import {action as Action} from "./action";
+export const count = (state=0, action) =>{
+    switch (action.type) {
+        case Action.INCREMENT :
+            return state +1;
+        case Action.DECREMENT :
+            return state-1;
+        default:
+            return state;
+    }
+};
---
+++ source/test/counter.spec.js
@@ -0,0 +1,15 @@
+import expect from "expect";
+import {count} from "../main/counter";
+import {action} from "../main/action";
+import {createStore} from "redux";
+
+describe("counter test", () => {
+    let store;
+    beforeEach(() => {
+        store = createStore(count);
+    });
+    it(("increment and decrement  should change the state correctly"), ()=> {
+      expect(store.getState()).toEqual(0);
+
+    })
+});
