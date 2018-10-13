4bb88a5e1d9432ad274ecefdf468b2bf893584c7
Sudhir Yadav <yadavsudhir405@gmail.com>
2018-10-13 17:02:23 +0530

    Displaying the missing starting value

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ import {createStore} from "redux";
 import {action} from "./source/main/action";
 
 const store = createStore(count);
+const render = ()=> {
+    document.body.innerText = store.getState();
+};
 
-store.subscribe(()=>{
-   document.body.innerText = store.getState();
-});
+render();
+store.subscribe(render);
 
 document.addEventListener("click", ()=>{
     store.dispatch({type: action.INCREMENT});
