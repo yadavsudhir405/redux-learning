0cc61a3dd9d8bb772cd0503376a890da433f044b
Sudhir Yadav <yadavsudhir405@gmail.com>
2018-11-07 13:36:14 +0530

    Experimenting with ract ref attribute

--- app/component/todo/todoApp.js
+++ app/component/todo/todoApp.js
@@ -7,6 +7,7 @@ export class TodoApp extends React.Component {
     constructor() {
         super();
         this.textInput = React.createRef();
+        this.focusTextField = this.focusTextField.bind(this);
     }
 
     render() {
@@ -22,6 +23,7 @@ export class TodoApp extends React.Component {
                 this.textInput.current.value = "";
             }}>Add Todo
             </button>
+            <input type="button" onClick={this.focusTextField()} value="Focus the text field"/>
             <ul>
                 {this.props.todos.map(todo =>
                     <li key={todo.id}>
@@ -29,5 +31,9 @@ export class TodoApp extends React.Component {
                     </li>)}
             </ul>
         </div>
+    }
+    focusTextField(){
+        console.log("Focusing the text field");
+        this.textInput.current.focus();
     }
 }
